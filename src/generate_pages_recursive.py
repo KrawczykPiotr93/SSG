0421f9b36827f4a78e68dc8c8e57af9c9605c5a1
KrawczykPiotr93 <krawczyk.piotr93@proton.me>
2026-01-07 17:49:45 +0100

    Kill me

--- src/generate_pages_recursive.py
+++ src/generate_pages_recursive.py
@@ -21,7 +21,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, con
 
             contents_in_html = markdown_to_html_node(markdown).to_html()
             title = extract_title(markdown)
-            template = template.replace("{{ Title }}", title).replace("{{ Content }}", contents_in_html).replace("href=/", f"href={basepath}").replace("src=/", f"src={basepath}")
+            template = template.replace("{{ Title }}", title).replace("{{ Content }}", contents_in_html).replace("href=/", f"href={basepath}").replace("src=/", f"src={basepath}").replace("href=\"/", f"href=\"{basepath}").replace("src=\"/", f"src=\"{basepath}")
             # print(template)
             
             dest_path = os.path.join(dest_dir_path, relative_path)
